ad19f6eaa63779684858579aa3018a454cb50f3b
NGUYEN Alexandre <alex.nr@hotmail.fr>
2015-12-01 22:44:52 +0100

    mega featured image design

--- wp-content/themes/blt-katla/index.php
+++ wp-content/themes/blt-katla/index.php
@@ -11,9 +11,12 @@
                 <div class="article-mega-featured">
 
                     <a href="<?php echo get_permalink($recent_posts[0]["post_id"]) ?>">
+
                         <div class="article-mega-featured-img-container">
                             <?php echo get_the_post_thumbnail($recent_posts[0]["post_id"], "attachment-large") ?>
-                            <h2><?php echo $recent_posts[0]["post_title"]; ?></h2>
+                            <div class="article-mega-featured-img">
+                                <h2><?php echo $recent_posts[0]["post_title"]; ?></h2>
+                            </div>
                         </div>
                     </a>
 
@@ -25,7 +28,9 @@
 
                     <div class="col-md-12 title-section">
                         <h3>DERNIERS ARTICLES</h3>
-                        <p>Marketing, art, innovation, CM...<br>Toutes les idées & concepts créatifs, amusants & inspirants du moment</p>
+
+                        <p>Marketing, art, innovation, CM...<br>Toutes les idées & concepts créatifs, amusants &
+                            inspirants du moment</p>
                     </div>
 
                 </div>
@@ -36,15 +41,17 @@
 
                         <?php $image = wp_get_attachment_image_src(get_post_thumbnail_id($post['ID']), 'single-post-thumbnail'); ?>
 
-                        <div class="col-md-6 article-vignette">
-                            <div class="article-vignette-inside-image"
-                                 style="background-image: url('<?php echo $image[0]; ?>')">
+                        <a href="<?php echo get_permalink($post['ID']); ?>">
+                            <div class="col-md-6 article-vignette">
+                                <div class="article-vignette-inside-image"
+                                     style="background-image: url('<?php echo $image[0]; ?>')">
 
+                                </div>
+                                <div class="article-vignette-inside-text">
+                                    <h4><?php echo $post["post_title"]; ?></h4>
+                                </div>
                             </div>
-                            <div class="article-vignette-inside-text">
-                                <h4><?php echo $post["post_title"]; ?></h4>
-                            </div>
-                        </div>
+                        </a>
 
                     <?php endforeach ?>
 
