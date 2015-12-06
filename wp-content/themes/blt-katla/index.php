cc16c45d20848b27ddc994d4fb109999fd539f6e
NGUYEN Alexandre <alex.nr@hotmail.fr>
2015-12-06 17:17:02 +0100

    links on article units are cross browsers

--- wp-content/themes/blt-katla/index.php
+++ wp-content/themes/blt-katla/index.php
@@ -14,7 +14,6 @@
                 <div class="article-mega-featured">
 
                     <a href="<?php echo get_permalink($recent_posts[0]->ID) ?>">
-
                         <div class="article-mega-featured-img-container">
                             <?php echo get_the_post_thumbnail($recent_posts[0]->ID, "attachment-large") ?>
                             <div class="article-mega-featured-title">
@@ -45,22 +44,24 @@
                         <?php $image = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'single-post-thumbnail'); ?>
 
                         <div class="col-md-6 article-vignette">
-                            <a href="<?php echo get_permalink($post->ID); ?>">
-                                <div class="article-vignette-inside-image"
-                                     style="background-image: url('<?php echo $image[0]; ?>')">
-                                    <div class="sharing-interactive" id="sharing-interactive-<?php echo $post->ID; ?>"
-                                         onmouseover="openSharePanelForID('<?php echo $post->ID; ?>')"
-                                         onmouseout="hideSharePanelForID('<?php echo $post->ID; ?>');">
-                                        <?php if (function_exists("social_shares_button")) social_shares_button("normal", $post->ID); ?>
-                                        <div id="post-share-box-<?php echo $post->ID; ?>" class="post-share-article">
-                                            <div class="fb-like" data-href="<?php echo get_permalink($post->ID); ?>"
-                                                 data-layout="button" data-action="like" data-show-faces="false"
-                                                 data-share="false"></div>
-                                            <a href="https://twitter.com/share" class="twitter-share-button" {count}
-                                               data-via="alex_zhixin">Tweet</a>
-                                        </div>
+                            <div class="article-vignette-inside-image"
+                                 style="background-image: url('<?php echo $image[0]; ?>')">
+                                <a href="<?php echo get_permalink($post->ID); ?>" class="home-article-background-link">
+                                </a>
+
+                                <div class="sharing-interactive" id="sharing-interactive-<?php echo $post->ID; ?>"
+                                     onmouseover="openSharePanelForID('<?php echo $post->ID; ?>')"
+                                     onmouseout="hideSharePanelForID('<?php echo $post->ID; ?>');">
+                                    <?php if (function_exists("social_shares_button")) social_shares_button("normal", $post->ID); ?>
+                                    <div id="post-share-box-<?php echo $post->ID; ?>" class="post-share-article">
+                                        <div class="fb-like" data-href="<?php echo get_permalink($post->ID); ?>"
+                                             data-layout="button" data-action="like" data-show-faces="false"
+                                             data-share="false"></div>
+                                        <a href="https://twitter.com/share" class="twitter-share-button" {count}>Tweet</a>
                                     </div>
                                 </div>
+                            </div>
+                            <a href="<?php echo get_permalink($post->ID); ?>">
                                 <div class="article-vignette-inside-text">
                                     <h4><?php echo $post->post_title; ?></h4>
                                 </div>
@@ -120,22 +121,24 @@
                         <?php $image = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'single-post-thumbnail'); ?>
 
                         <div class="col-md-6 article-vignette">
-                            <a href="<?php echo get_permalink($post->ID); ?>">
-                                <div class="article-vignette-inside-image"
-                                     style="background-image: url('<?php echo $image[0]; ?>')">
-                                    <div class="sharing-interactive" id="sharing-interactive-<?php echo $post->ID; ?>"
-                                         onmouseover="openSharePanelForID('<?php echo $post->ID; ?>')"
-                                         onmouseout="hideSharePanelForID('<?php echo $post->ID; ?>');">
-                                        <?php if (function_exists("social_shares_button")) social_shares_button("normal", $post->ID); ?>
-                                        <div id="post-share-box-<?php echo $post->ID; ?>" class="post-share-article">
-                                            <div class="fb-like" data-href="<?php echo get_permalink($post->ID); ?>"
-                                                 data-layout="button" data-action="like" data-show-faces="false"
-                                                 data-share="false"></div>
-                                            <a href="https://twitter.com/share" class="twitter-share-button" {count}
-                                               data-via="alex_zhixin">Tweet</a>
-                                        </div>
+                            <div class="article-vignette-inside-image"
+                                 style="background-image: url('<?php echo $image[0]; ?>')">
+                                <a href="<?php echo get_permalink($post->ID); ?>" class="home-article-background-link">
+                                </a>
+
+                                <div class="sharing-interactive" id="sharing-interactive-<?php echo $post->ID; ?>"
+                                     onmouseover="openSharePanelForID('<?php echo $post->ID; ?>')"
+                                     onmouseout="hideSharePanelForID('<?php echo $post->ID; ?>');">
+                                    <?php if (function_exists("social_shares_button")) social_shares_button("normal", $post->ID); ?>
+                                    <div id="post-share-box-<?php echo $post->ID; ?>" class="post-share-article">
+                                        <div class="fb-like" data-href="<?php echo get_permalink($post->ID); ?>"
+                                             data-layout="button" data-action="like" data-show-faces="false"
+                                             data-share="false"></div>
+                                        <a href="https://twitter.com/share" class="twitter-share-button" {count}>Tweet</a>
                                     </div>
                                 </div>
+                            </div>
+                            <a href="<?php echo get_permalink($post->ID); ?>">
                                 <div class="article-vignette-inside-text">
                                     <h4><?php echo $post->post_title; ?></h4>
                                 </div>
@@ -165,22 +168,24 @@
                         <?php $image = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'single-post-thumbnail'); ?>
 
                         <div class="col-md-6 article-vignette">
-                            <a href="<?php echo get_permalink($post->ID); ?>">
-                                <div class="article-vignette-inside-image"
-                                     style="background-image: url('<?php echo $image[0]; ?>')">
-                                    <div class="sharing-interactive" id="sharing-interactive-<?php echo $post->ID; ?>"
-                                         onmouseover="openSharePanelForID('<?php echo $post->ID; ?>')"
-                                         onmouseout="hideSharePanelForID('<?php echo $post->ID; ?>');">
-                                        <?php if (function_exists("social_shares_button")) social_shares_button("normal", $post->ID); ?>
-                                        <div id="post-share-box-<?php echo $post->ID; ?>" class="post-share-article">
-                                            <div class="fb-like" data-href="<?php echo get_permalink($post->ID); ?>"
-                                                 data-layout="button" data-action="like" data-show-faces="false"
-                                                 data-share="false"></div>
-                                            <a href="https://twitter.com/share" class="twitter-share-button" {count}
-                                               data-via="alex_zhixin">Tweet</a>
-                                        </div>
+                            <div class="article-vignette-inside-image"
+                                 style="background-image: url('<?php echo $image[0]; ?>')">
+                                <a href="<?php echo get_permalink($post->ID); ?>" class="home-article-background-link">
+                                </a>
+
+                                <div class="sharing-interactive" id="sharing-interactive-<?php echo $post->ID; ?>"
+                                     onmouseover="openSharePanelForID('<?php echo $post->ID; ?>')"
+                                     onmouseout="hideSharePanelForID('<?php echo $post->ID; ?>');">
+                                    <?php if (function_exists("social_shares_button")) social_shares_button("normal", $post->ID); ?>
+                                    <div id="post-share-box-<?php echo $post->ID; ?>" class="post-share-article">
+                                        <div class="fb-like" data-href="<?php echo get_permalink($post->ID); ?>"
+                                             data-layout="button" data-action="like" data-show-faces="false"
+                                             data-share="false"></div>
+                                        <a href="https://twitter.com/share" class="twitter-share-button" {count}>Tweet</a>
                                     </div>
                                 </div>
+                            </div>
+                            <a href="<?php echo get_permalink($post->ID); ?>">
                                 <div class="article-vignette-inside-text">
                                     <h4><?php echo $post->post_title; ?></h4>
                                 </div>
