3fae3f983fb4b4e625fc69ba8806485415d87307
NGUYEN Alexandre <alex.nr@hotmail.fr>
2016-01-09 18:14:53 +0100

    updated header article

--- wp-content/plugins/SPECIFIC-DEV-PIWEE/js/script.js
+++ wp-content/plugins/SPECIFIC-DEV-PIWEE/js/script.js
@@ -60,7 +60,6 @@ jQuery(document).ready(function () {
 
     }
 
-
     jQuery(".img-pub-right").scrollToFixed({
         preFixed: function () {
 
--- wp-content/themes/blt-katla/inc/template-parts/single.php
+++ wp-content/themes/blt-katla/inc/template-parts/single.php
@@ -9,40 +9,40 @@
 
     <h1 class="single-title"><?php the_title(); ?></h1>
 
-    <div class="author-container">
 
-        <?php
-            echo get_avatar(get_the_author_id());
-            echo '<a href="/' . get_the_author_link() . '">' . get_the_author() . '</a><br>';
-            the_date();
-        ?>
+    <div class="header-article">
 
-    </div>
+        <div class="row">
+            <div class="col-md-4 col-sm-8 col-xs-8">
+                <div class="author-container">
 
-    <div class="share-container header-article">
+                    <?php
+                    echo get_avatar(get_the_author_id());
+                    echo '<a href="/' . get_the_author_link() . '">' . get_the_author() . '</a><br>';
+                    the_date();
+                    ?>
 
-        <div class="share-count">
-            <span class="share-count-views">11K</span>
-            <br>
-            Partages
-        </div>
-        <div class="share-share">
-            <img src="<?php echo get_template_directory_uri() ?>/assets/img/piwee-icon/fb.png">
-            <img src="<?php echo get_template_directory_uri() ?>/assets/img/piwee-icon/twitter.png">
-            <img src="<?php echo get_template_directory_uri() ?>/assets/img/piwee-icon/gplus.png">
-            <img src="<?php echo get_template_directory_uri() ?>/assets/img/piwee-icon/linkedin.png">
-            <img src="<?php echo get_template_directory_uri() ?>/assets/img/piwee-icon/pinterest.png">
-        </div>
-        <div class="share-postvote hidden-sm hidden-xs">
-            <div class="percent-container">
-                <span class="share-percent">60%</span>
-                <br>
-                Génie
-                <img src="<?php echo get_template_directory_uri() ?>/assets/img/piwee-icon/icon-genie.png">
+                </div>
+            </div>
+            <div class="col-md-8 col-sm-4 col-xs-4">
+                <div class="share-container">
+                    <div class="share-count">
+                        <span class="share-count-views">11K</span>
+                        <br>
+                        Partages
+                    </div>
+                    <div class="share-buttons">
+                        <img class="hidden-xs hidden-sm" src="<?php echo get_template_directory_uri() ?>/assets/img/piwee-icon/flat-con-piwee-fb-btn.png">
+                        <img class="hidden-xs hidden-sm" src="<?php echo get_template_directory_uri() ?>/assets/img/piwee-icon/flat-con-piwee-twitter-btn.png">
+                        <img class="hidden-xs hidden-sm" src="<?php echo get_template_directory_uri() ?>/assets/img/piwee-icon/flat-con-piwee-linkedin.png">
+                        <img class="hidden-xs hidden-sm" src="<?php echo get_template_directory_uri() ?>/assets/img/piwee-icon/flat-con-piwee-pinterest.png">
+                        <img class="hidden-xs hidden-sm" src="<?php echo get_template_directory_uri() ?>/assets/img/piwee-icon/flat-con-piwee-mail.png">
+                        <img class="hidden-md hidden-lg" src="<?php echo get_template_directory_uri() ?>/assets/img/piwee-icon/flat-con-piwee-fb.png">
+                        <img class="hidden-md hidden-lg" src="<?php echo get_template_directory_uri() ?>/assets/img/piwee-icon/flat-con-piwee-twitter.png">
+                    </div>
+                </div>
             </div>
         </div>
-
-        <div style="clear:both"></div>
 
     </div>
 
