99cf55b32d1135463e5bf5685f74a57697bc0afd
NGUYEN Alexandre <alex.nr@hotmail.fr>
2016-01-20 22:18:40 +0100

    clean useless script in single.php

--- wp-content/themes/blt-katla/inc/template-parts/single.php
+++ wp-content/themes/blt-katla/inc/template-parts/single.php
@@ -1,14 +1,6 @@
 <article <?php post_class(); ?>>
 
-    <?php
-    if (is_sticky()) {
-        echo '<p class="label-wrap"><span class="label label-blt label-sticky"><i class="fa fa-star"></i>' . __('Featured Post', 'bluthemes') . '</span></p>';
-    }
-    ?>
-
-
     <h1 class="single-title"><?php the_title(); ?></h1>
-
 
     <div class="header-article">
 
@@ -32,13 +24,20 @@
                         Partages
                     </div>
                     <div class="share-buttons">
-                        <img class="hidden-xs hidden-sm" src="<?php echo get_template_directory_uri() ?>/assets/img/piwee-icon/flat-con-piwee-fb-btn.png">
-                        <img class="hidden-xs hidden-sm" src="<?php echo get_template_directory_uri() ?>/assets/img/piwee-icon/flat-con-piwee-twitter-btn.png">
-                        <img class="hidden-xs hidden-sm" src="<?php echo get_template_directory_uri() ?>/assets/img/piwee-icon/flat-con-piwee-linkedin.png">
-                        <img class="hidden-xs hidden-sm" src="<?php echo get_template_directory_uri() ?>/assets/img/piwee-icon/flat-con-piwee-pinterest.png">
-                        <img class="hidden-xs hidden-sm" src="<?php echo get_template_directory_uri() ?>/assets/img/piwee-icon/flat-con-piwee-mail.png">
-                        <img class="hidden-md hidden-lg" src="<?php echo get_template_directory_uri() ?>/assets/img/piwee-icon/flat-con-piwee-fb.png">
-                        <img class="hidden-md hidden-lg" src="<?php echo get_template_directory_uri() ?>/assets/img/piwee-icon/flat-con-piwee-twitter.png">
+                        <img class="hidden-xs hidden-sm"
+                             src="<?php echo get_template_directory_uri() ?>/assets/img/piwee-icon/flat-con-piwee-fb-btn.png">
+                        <img class="hidden-xs hidden-sm"
+                             src="<?php echo get_template_directory_uri() ?>/assets/img/piwee-icon/flat-con-piwee-twitter-btn.png">
+                        <img class="hidden-xs hidden-sm"
+                             src="<?php echo get_template_directory_uri() ?>/assets/img/piwee-icon/flat-con-piwee-linkedin.png">
+                        <img class="hidden-xs hidden-sm"
+                             src="<?php echo get_template_directory_uri() ?>/assets/img/piwee-icon/flat-con-piwee-pinterest.png">
+                        <img class="hidden-xs hidden-sm"
+                             src="<?php echo get_template_directory_uri() ?>/assets/img/piwee-icon/flat-con-piwee-mail.png">
+                        <img class="hidden-md hidden-lg"
+                             src="<?php echo get_template_directory_uri() ?>/assets/img/piwee-icon/flat-con-piwee-fb.png">
+                        <img class="hidden-md hidden-lg"
+                             src="<?php echo get_template_directory_uri() ?>/assets/img/piwee-icon/flat-con-piwee-twitter.png">
                     </div>
                 </div>
             </div>
@@ -46,35 +45,8 @@
 
     </div>
 
-    <div class="single-text"><?php
+    <div class="post">
+        <?php the_content(); ?>
+    </div>
 
-        the_content(sprintf(
-            __('Continue reading %s', 'bluthemes'),
-            the_title('<span class="screen-reader-text">', '</span>', false)
-        ));
-
-
-        // Paginated posts
-        wp_link_pages(array('before' => '<nav class="blu-post-pagination" role="navigation"><strong>' . __('Pages:', 'bluthemes') . '</strong>', 'link_before' => '<span>', 'after' => '</nav>', 'link_after' => '</span>', 'pagelink' => '%'));
-
-        ?>
-    </div><?php
-
-    blt_post_thumbnail();
-
-    // AD SPOT #5
-    blt_get_ad_spot('spot_below_content');
-
-    // Tags
-    the_tags('<div class="blu-post-tags"><strong>' . __('Tags:', 'bluthemes') . '</strong><ul><li>', '</li><li>', '</li></ul></div>');
-
-    // More Info URL
-    if (get_field('blt_info_url')) { ?>
-        <hr>
-        <strong><a href="<?php echo get_field('blt_info_url'); ?>"
-                   target="_blank"><?php echo get_field('blt_info_name') ? get_field('blt_info_name') : __('More Info'); ?></a>
-        </strong><?php
-    }
-
-    ?>
 </article>
