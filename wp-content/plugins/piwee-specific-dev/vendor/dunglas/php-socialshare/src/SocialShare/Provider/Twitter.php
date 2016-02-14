8398ac37da045d8c35863d968c3faaff5cd8f887
NGUYEN Alexandre <alex.nr@hotmail.fr>
2016-02-14 21:19:28 +0100

    replaced twitter API URL

--- wp-content/plugins/piwee-specific-dev/vendor/dunglas/php-socialshare/src/SocialShare/Provider/Twitter.php
+++ wp-content/plugins/piwee-specific-dev/vendor/dunglas/php-socialshare/src/SocialShare/Provider/Twitter.php
@@ -20,7 +20,7 @@ class Twitter implements ProviderInterface
 {
     const NAME = 'twitter';
     const SHARE_URL = 'https://twitter.com/intent/tweet?%s';
-    const API_URL = 'http://opensharecount.com/count.json?url=%s';
+    const API_URL = 'http://public.newsharecounts.com/count.json?url=%s';
 
     /**
      * {@inheritdoc}
